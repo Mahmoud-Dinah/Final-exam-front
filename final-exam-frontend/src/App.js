cca4e461bf3c2f6f6cae44ebf1aa6d85e915fcd3
root <root@Mahmood.localdomain>
2021-07-06 12:16:17 +0300

    added header

---
+++ final-exam-frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Header />
+              <Switch>
+          <Route path="/">
+            {/* <Main /> */}
+          </Route>
+          <Route path="/favorite">
+            {/* <Favorite /> */}
+          </Route>
+        </Switch>
+        </Router>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ final-exam-frontend/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+              <h1>Hello from the header</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
